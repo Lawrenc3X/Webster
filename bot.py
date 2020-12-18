9beb67d2a4a9bf13e510171586274357627dd049
Lawrenc3X <57817583+Lawrenc3X@users.noreply.github.com>
2020-12-18 01:43:44 -0800

    Cleaning

--- bot.py
+++ bot.py
@@ -1,37 +1,31 @@
 # bot.py
 import os
-from typing import Generator
 from re import sub
 from pprint import pprint
 
-import discord
+import discord  # install
 from discord.ext import commands
-from dotenv import load_dotenv
 from spellchecker import SpellChecker
 from spamprotector import SpamProtector
-from customdictionary import UserDictionary
 
-load_dotenv()
-# TOKEN = os.getenv('DISCORD_TOKEN')
-TOKEN = os.environ['DISCORD_TOKEN']
+TOKEN = os.environ['']  # fill
 
 bot = commands.Bot(command_prefix='w!')
-spellcheckers = {}
 spam = SpamProtector()
+spell = SpellChecker()
 previous_message = None
 
-def corrections(author) -> Generator[str, None, None]:
-    if author in spellcheckers:
-        spell = spellcheckers[author]
-    else:
-        spellcheckers[author] = UserDictionary()
-        spell = spellcheckers[author]
 
-    words = [sub("(^[^a-zA-Z]+|[^a-zA-Z]+$)", "", word) for word in previous_message.split(" ")]
+def corrections(author):
+    words = [
+        sub("(^[^a-zA-Z]+|[^a-zA-Z]+$)", "", word)
+        for word in previous_message.split(" ")
+    ]
     misspelled = spell.unknown(words)
 
     for word in misspelled:
         yield spell.correction(word)
+
 
 @bot.event
 async def on_message(message):
@@ -47,32 +41,23 @@ async def on_message(message):
             print(len(spam.requests[message.author.id]))
             pprint(spam.requests)
 
-            if previous_message == None:
+            if previous_message is None:
                 await message.channel.send("no recent messages")
                 return
 
-            await message.channel.send("*" + ", ".join(corrections(message.author.id) ) )
+            await message.channel.send(
+                "*" + ", ".join(corrections(message.author.id))
+            )
 
     else:
         previous_message = message.content
- 
+
     await bot.process_commands(message)
 
-@bot.command(name = "add")
-async def add(ctx, word = None):
-    """add words to your dictionary"""
-    if word == None:
-        print("no word")
-        await ctx.send("usage: w!add [word]")
-        return
-    
-    if ctx.author.id in spellcheckers:
-        spellcheckers[ctx.author.id].add(word)
-    else:
-        spellcheckers[ctx.author.id] = UserDictionary()
-        spellcheckers[ctx.author.id].add(word) 
 
-    print("invoked")
-    await ctx.send("placeholder command")
+@bot.command(name="add")
+async def add(ctx, word=None):
+    """add words to your dictionary"""
+    pass
 
 bot.run(TOKEN)
--- spamprotector.py
+++ spamprotector.py
@@ -1,18 +1,19 @@
 from datetime import datetime, timedelta
 
+
 class SpamProtector(object):
     """protects the bot from too many requests"""
-    def __init__(self, ignore_period = 15, spam_window = 30, request_limit = 10):
+    def __init__(self,
+                 ignore_period=15, spam_window=30, request_limit=10):
         self.ignore_period = ignore_period
         self.spam_window = spam_window
         self.request_limit = request_limit
 
-        
         self.requests = {}  # userid : request timestamps list
         self.ignored = {}   # userid : when we stop ignorinng them
 
     def ignore(self, user):
-        """returns true if this user should be ignored and updates other information"""
+        """returns true if this user should be ignored"""
         if user not in self.requests:
             return False
 
@@ -24,20 +25,21 @@ class SpamProtector(object):
             return True
 
         else:
-            earliest = datetime.now() - timedelta(seconds = self.spam_window)
-            
+            earliest = datetime.now() - timedelta(seconds=self.spam_window)
+
             for index, time in enumerate(self.requests[user]):
                 if time >= earliest:
                     self.requests[user] = self.requests[user][index:]
                     break
 
             if len(self.requests[user]) >= self.request_limit:
-                self.ignored[user] = datetime.now() + timedelta(seconds = self.ignore_period)
+                self.ignored[user] = (
+                        datetime.now() + timedelta(seconds=self.ignore_period))
                 return True
             return False
 
     def log(self, user):
-        """logs a request"""        
+        """logs a request"""
         if user in self.requests:
             self.requests[user].append(datetime.now())
         else:
